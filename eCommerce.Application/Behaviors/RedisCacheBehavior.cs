79c65c07baff83c0c00d92fffa6d11aad6e70cb6
eselk04 <emreselek04@gmail.com>
2025-03-14 22:45:11 +0300

    redis

---
+++ eCommerce.Application/Behaviors/RedisCacheBehavior.cs
@@ -0,0 +1,33 @@
+using eCommerce.Application.Interfaces.RedisCache;
+using MediatR;
+
+namespace eCommerce.Application.Behaviors;
+
+public class RedisCacheBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+{
+    private readonly IRedisCacheService redisCacheService;
+
+    public RedisCacheBehavior(IRedisCacheService redisCacheService)
+    {
+        this.redisCacheService = redisCacheService;
+    } 
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        if (request is ICacheableQuery query)
+        {
+            var cacheKey = query.CacheKey;
+            var cacheTime = query.CacheTime;
+
+
+            var cacheData = await redisCacheService.GetAsync<TResponse>(cacheKey);
+            if(cacheData is not null)
+                return cacheData;
+            
+            var response = await next();
+            if (response is not null)
+                await redisCacheService.SetAsync(cacheKey, response, DateTime.Now.AddMinutes(cacheTime));
+            return response;
+        }
+        return await next();
+    }
+}
